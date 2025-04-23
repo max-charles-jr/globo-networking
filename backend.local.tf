52fa444bccc64a404aef6909c86ec3b36c7cd508
Max Charles Jr. <mcharles1682@gmail.com>
2025-04-22 20:20:57 -0400

    Add CI workflow

--- backend.local.tf
+++ backend.local.tf
@@ -1,11 +1,11 @@
 ## Move this backend file to your network config when migrating state
-terraform {
-  cloud {
-    # Organization ID
-    organization = "max-charles-jr"
-    # Workspace ID
-    workspaces {
-      name = "web-network-dev"
-    }
-  }
-}
+# terraform {
+#   cloud {
+#     # Organization ID
+#     organization = "max-charles-jr"
+#     # Workspace ID
+#     workspaces {
+#       name = "web-network-dev"
+#     }
+#   }
+# }
